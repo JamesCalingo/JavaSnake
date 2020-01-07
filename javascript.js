f514eaee72b20de04dd3dd6fa7e482ddb2cad9e2
JC <james.calingo@gmail.com>
2020-01-07 16:51:04 -0500

    working on JavaScript

--- javascript.js
+++ javascript.js
@@ -1,7 +1,39 @@
-var canvas = document.body.children[0];
+var canvas = document.body.children[2];
 var context = canvas.getContext("2d");
 context.scale(10, 10);
 // context.fillRect(0, 0, 10, 10);
+
+// Movement using arrow keys
+document.onkeyup = function(e){
+  // // DOWN
+  // if(e.which == 38){
+  //   direction = [0, -1];
+  //   // UP
+  // }else if (e.which = 40){
+      // direction = [0, 1]
+  // }
+switch (e.which){
+  case 37:
+    // LEFT
+    direction = [-1, 0]
+    break;
+  
+  case 38:
+    // DOWN
+    direction = [0, -1]
+    break;
+
+  case 39:
+    // RIGHT
+    direction = [1, 0]
+    break;
+
+  case 40:
+    // UP
+    direction = [0, 1]
+    break
+}
+}
 
 var snake = [
   [0,1],
@@ -16,13 +48,20 @@ updateLoop = () =>{
 }
 
 draw = () => {
+  context.clearRect(0, 0, Infinity, Infinity)
 snake.forEach(function([x, y]) {
   context.fillRect(x, y, 1, 1)
 })
 }
+
+draw()
 var keyboardState = [0,0]
 
 keyboardState = [0,1]
-tail = snake.pop()
-tail[0] += keyboardState[0]
-tail[1] += keyboardState[1]
+tail = snake.pop();
+head = snake[0]
+tail[0] = head[0] + keyboardState[0]
+tail[1] = head[1] + keyboardState[1]
+snake.unshift(tail)
+console.log(snake)
+draw()
