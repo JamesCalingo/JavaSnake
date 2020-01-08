b17b54166ecfe9d22d002c438a7c16aa0d5dd721
JC <james.calingo@gmail.com>
2020-01-08 11:23:52 -0500

    game works mostly and changed css

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 var canvas = document.body.children[2];
 var context = canvas.getContext("2d");
 context.scale(10, 10);
-// context.fillRect(0, 0, 10, 10);
+
 
 // Movement using arrow keys
 document.onkeyup = function(e){
@@ -31,33 +31,66 @@ switch (e.key){
     break
 }
 }
-
+// Game components
 var snake = [
-  [0,1],
-  [0,2],
-  [0,3],
-  [1,3],
-  [1,4]
+  [0,0],
+  [1,0],
+  [2,0],
+  [3,0],
+  [4,0]
 ];
 
+var direction = [1,0]
+var apple = [Math.floor(Math.random() * 30) | 0,
+  Math.floor(Math.random() * 30) | 0]
+
+// Rendering the game to the page
 updateLoop = () =>{
-  tail = snake.pop();
-  head = snake[0];
+  let head = snake[0]
   console.log(snake)
-  tail[0] = head[0] + direction[0];
-  tail[1] = head[1] + direction[1];
-  snake.unshift(tail);
+  snake.unshift([
+    snake[0][0] + direction[0],
+    snake[0][1] + direction[1]
+  ]);
+
+  if(snake[0][0] == apple[0] &&
+    snake[0][1] == apple[1]){
+    apple = [Math.floor(Math.random() * 30) | 0,
+      Math.floor(Math.random() * 30) | 0]
+  }else{
+    snake.pop()
+  }
   draw();
 }
 
 draw = () => {
   context.clearRect(0, 0, 300, 300)
+
+  context.fillStyle="red"
+context.fillRect(apple[0], apple[1], 1, 1)
+
+  context.fillStyle = "green"
 snake.forEach(function([x, y]) {
   context.fillRect(x, y, 1, 1)
 })
+
+
 }
 
 draw()
-var direction = [1,0]
 
-setInterval(updateLoop, 800)
+
+setInterval(updateLoop, 300);
+
+
+// Game Over Conditions
+for (let i=1; i < snake.length; i++){
+  if(snake[0][0] == snake[i][0]){
+    clearInterval(updateLoop)
+    alert("BANG")
+  }
+}
+if(snake[0][0] > 30 || snake[0][0] < 0){
+  clearInterval(updateLoop)
+  alert("BANG")
+}
