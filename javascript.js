433496b884be5bb1b6204a937165c5fbe2eb0430
JC <james.calingo@gmail.com>
2020-01-13 16:51:00 -0500

    stop game on game over

--- javascript.js
+++ javascript.js
@@ -2,13 +2,14 @@ var canvas = document.body.children[2];
 var context = canvas.getContext("2d");
 context.scale(10, 10);
 var score = 0;
-var highScore = 0
+var highScore = 0;
+var intervalID;
 
 Swal.fire({
   title: "Snake? SNAAAAAAKE?!",
   text: "Welcome to one of the most iconic digital games from the early days of phones - Snake! Use the arrow keys to move the green snake around the board, collecting the red dots as you go. Each red dot you get adds one point to your score and makes the snake longer. Be careful, though - if the head of the snake hits the edges of the board or another part of its body, then the game ends.",
   confirmButtonText: "Let's go!"
-}).then(() => {setInterval(updateLoop, 300)})
+}).then(() => {intervalID = setInterval(updateLoop, 300)})
 
 // Movement using arrow keys
 document.onkeyup = function(e){
@@ -43,16 +44,16 @@ var snake = [
   [10,14],
   [10,15]
 ];
-
+// Initial direction and apple position
 var direction = [1,0];
-var apple = [17,25];
+var apple = [10, 13];
 
 reset = () => {
-  clearInterval(updateLoop);
+  clearInterval(intervalID);
   Swal.fire({
     title: "Game Over!",
     text: `Your score was ${score}. The current high score is ${highScore}. Try again?`
-  })
+  }).then(() => {intervalID = setInterval(updateLoop, 300)})
   score = 0;
   snake = [
     [10,11],
@@ -62,9 +63,10 @@ reset = () => {
     [10,15]
   ];
 direction = [1,0];
-apple = [17,25];
+apple = [10,13];
 $("#score").html(0)
 }
+
 // Rendering the game to the page
 updateLoop = () =>{
   console.log(snake)
@@ -85,26 +87,25 @@ updateLoop = () =>{
   }else{
     snake.pop()
   }
+  for(let i = 1; i < snake.length; i++){
+    if(apple[0] == snake[i][0] && apple[1] == snake[i][1]){
+     apple = [Math.floor(Math.random() * 30) | 0,
+      Math.floor(Math.random() * 30) | 0]
+  
+  }
+}
 // Game Over Conditions
-
-
 for(let i = 1; i < snake.length; i++){
   if(snake[0][0] == snake[i][0] && snake[0][1] == snake[i][1]){
-   
-  
     reset()
   }
   if(snake[0][0] >= 30 || snake[0][0] < 0){
- 
     reset()
   }
   if(snake[0][1] >= 30 || snake[0][1] < 0){
-  
     reset()
   }
-
 }
-
   draw();
 }
 
@@ -118,20 +119,6 @@ context.fillRect(apple[0], apple[1], 1, 1)
 snake.forEach(function([x, y]) {
   context.fillRect(x, y, 1, 1)
 })
-
-
 }
-
+// This draw function is to make sure the game initializes
 draw()
-
-
-// setInterval(updateLoop, 300);
-
-
-// Game Over Conditions
-// for (let i=1; i < snake.length; i++){
-//   if(snake[0][0] == snake[i][0]){
-//     clearInterval(updateLoop)
-//     alert("BANG")
-//   }
-// }
