40e4376cb809a65926ceaf318d1e9665b72aeda3
JC <james.calingo@gmail.com>
2020-01-14 10:40:05 -0500

    css, font changes

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ Swal.fire({
   title: "Snake? SNAAAAAAKE?!",
   text: "Welcome to one of the most iconic digital games from the early days of phones - Snake! Use the arrow keys to move the green snake around the board, collecting the red dots as you go. Each red dot you get adds one point to your score and makes the snake longer. Be careful, though - if the head of the snake hits the edges of the board or another part of its body, then the game ends.",
   confirmButtonText: "Let's go!"
-}).then(() => {intervalID = setInterval(updateLoop, 300)})
+}).then(() => {intervalID = setInterval(updateLoop, 420)})
 
 // Movement using arrow keys
 document.onkeyup = function(e){
@@ -53,7 +53,7 @@ reset = () => {
   Swal.fire({
     title: "Game Over!",
     text: `Your score was ${score}. The current high score is ${highScore}. Try again?`
-  }).then(() => {intervalID = setInterval(updateLoop, 300)})
+  }).then(() => {intervalID = setInterval(updateLoop, 420)})
   score = 0;
   snake = [
     [10,11],
